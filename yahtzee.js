27b07d3f3dbadd54009bc318718caae758d2c40e
Maybe <Lop7777@users.noreply.github.com>
2023-06-15 23:18:04 +0900

    End_Project_Final

--- yahtzee.js
+++ yahtzee.js
@@ -3,108 +3,319 @@ const $$player1 = document.querySelectorAll('[player-01]');
 const $$player2 = document.querySelectorAll('[player-02]');
 const $$subtotal = document.querySelectorAll('.subtotal');
 const $$bonus = document.querySelectorAll('.bonus');
+const $$total = document.querySelectorAll('.total');
 
-let diceValue = [0, 0, 0, 0, 0]; // 주사위 5개 각 숫자 (굴릴 때마다 변화)
-let diceBool = [true, true, true, true, true]; // 주사위 고정 or 해제
+let diceValue = [1, 1, 1, 1, 1];
+let diceBool = [true, true, true, true, true];
 
-let playerboard = [{1: NaN, 2: NaN, 3: NaN, 4: NaN, 5: NaN, 6: NaN},
-              {1: NaN, 2: NaN, 3: NaN, 4: NaN, 5: NaN, 6: NaN}]; // player 스코어 (!뒤에 7번, 8번... 추가하여 확장!)
-let tmpboard = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0}; // 주사위 굴릴 때 숫자 임시 기록 (!playerscore과 동일 할 것!)
-let playerTotal = [{subtotal : 0, bonus : 0, total : 0}, {subtotal : 0, bonus : 0, total : 0}]
+let playerboard = [{1: NaN, 2: NaN, 3: NaN, 4: NaN, 5: NaN, 6: NaN, 7: NaN, 8: NaN, 9: NaN, 10: NaN, 11: NaN, 12: NaN},
+                   {1: NaN, 2: NaN, 3: NaN, 4: NaN, 5: NaN, 6: NaN, 7: NaN, 8: NaN, 9: NaN, 10: NaN, 11: NaN, 12: NaN}];
+let tmpboard = {};
+let playerTotal = [{subtotal : 0, maintotal : 0, bonus : 0, total : 0}, {subtotal : 0, maintotal : 0, bonus : 0, total : 0}]
 
 let TURN = 0;
-let rollCount = 3; // 굴릴 수 있는 횟수
+let rollCount = 3;
+let selectTurn = 0;
+let diceFlag = false;
 
 document.querySelector('#roll').addEventListener('click', () => {
-    if(rollCount == 0) return; // 3번 굴리면 무시
+    if(diceFlag) return;
+    if(rollCount === 0) return;
 
-    tmpboard = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0}; // 점수 기록판 비움
+    diceFlag = true;
+    const interval = setInterval(() => {
+        radomizeDice(diceContainer);
+    }, 50);
 
-    for(let i = 0; i < 5; i++) { // 주사위 굴림
-        if(diceBool[i]) { // diceBool 이 true로 활성화 되어 있을 때만 실행
-            diceValue[i] = Math.floor(Math.random() * 6) + 1;
-            $$dice[i].innerHTML = diceValue[i];
+    setTimeout(() => {
+        clearInterval(interval)
+        diceFlag = false;
+
+        tmpboard = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 8: 0, 9: 0, 10: 0, 11: 0, 12: 0};
+        for(let i = 0; i < 5; i++) {
+            if(diceBool[i]) {
+                diceValue[i] = Math.floor(Math.random() * 6) + 1;
+                $$dice[i].innerHTML = diceValue[i];
+            }
         }
-    }
+        
+        //diceValue = [1,1,1,1,1];
 
-    diceValue.forEach(value => { // 기록판 (Aces ~ Sixes) 까지 기록
-        tmpboard[value] += value; // 주사위 값이 기록된 배열 순회하며 값이 X인 경우 board.X에 가산
-    });
+        setDice(diceContainer);
 
-    //!!여기에 fourkind, fullhouse 등은 어떻게 처리할 지 작성하면 됨!!
+        diceValue.forEach(value => {
+            tmpboard[value] += value;
+        });
 
-    for(let j = 0; j < 6; j++) { // 표 숫자를 board 내부 값에 따라 변경
-        if(TURN === 0) {
-            if (!isNaN(playerboard[TURN][j + 1])) continue;
-            $$player1[j].innerHTML = tmpboard[j+1];
+        diceValue.forEach(value => {
+            tmpboard[7] += value;
+        })
+        
+        const values = new Set(diceValue);
+
+        if(values.size === 1 || values.size === 2) {
+            values.forEach(v1 => {
+                let count = 0;
+                diceValue.forEach(v2 => {
+                    if(v1 === v2) {
+                        count ++;
+                    }
+                });
+                if (count >= 4) {
+                    tmpboard[8] = tmpboard[7];
+                    return;
+                }
+            })
         }
-        else if (TURN === 1) {
-            if (!isNaN(playerboard[TURN][j + 1])) continue;
-            $$player2[j].innerHTML = tmpboard[j+1];
-        }
-    }
 
-    rollCount -= 1; // 굴릴 수 있는 횟수 깎기
+        let valueCounts = {};
+        diceValue.forEach(value => {
+            if (valueCounts[value]) {
+                valueCounts[value]++;
+            } else {
+                valueCounts[value] = 1;
+            }
+        });
+
+        let hasTwo = false;
+        let hasThree = false;
+        for (let value in valueCounts) {
+            if (valueCounts[value] === 2) {
+                hasTwo = true;
+            } else if (valueCounts[value] === 3) {
+                hasThree = true;
+            }
+        }
+
+        if (hasTwo && hasThree) {
+            tmpboard[9] = tmpboard[7];
+        }
+
+        if (values.has(1) && values.has(2) && values.has(3) && values.has(4)) {
+            tmpboard[10] = 15;
+        } else if (values.has(2) && values.has(3) && values.has(4) && values.has(5)) {
+            tmpboard[10] = 15;
+        } else if (values.has(3) && values.has(4) && values.has(5) && values.has(6)) {
+            tmpboard[10] = 15;
+        }
+
+        if (values.has(1) && values.has(2) && values.has(3) && values.has(4) && values.has(5)) {
+            tmpboard[11] = 30;
+        } else if (values.has(2) && values.has(3) && values.has(4) && values.has(5) && values.has(6)) {
+            tmpboard[11] = 30;
+        }
+
+        if (values.size === 1) {
+            tmpboard[12] = 50;
+        }
+
+        for(let j = 0; j < Object.keys(tmpboard).length; j++) {
+            if(TURN === 0) {
+                if (!isNaN(playerboard[TURN][j + 1])) continue;
+                $$player1[j].innerHTML = tmpboard[j+1];
+            }
+            else if (TURN === 1) {
+                if (!isNaN(playerboard[TURN][j + 1])) continue;
+                $$player2[j].innerHTML = tmpboard[j+1];
+            }
+        }
+        rollCount --;
+        
+    }, 1000);
+
 })
 
-$$dice.forEach((element, index) => { // 주사위 클릭시 고정할 지 여부
-    element.addEventListener("click", () => {
-        if(diceValue[0] === 0) { // 주사위 내부 값이 비어있으면 (아무 배열값이라도 0이면 전부 0일테니 0번째만 체크) 리턴 
-            return;
-        }
-        if(diceBool[index]) {
-            element.style.backgroundColor = 'grey';
-            diceBool[index] = false;
-        }
-        else {
-            element.style.backgroundColor = 'white';
-            diceBool[index] = true;
-        }
-    })
-})
-
-const Select = (element, index) => { // 표 선택 시 동작 (바로 밑 이벤트 리스너로 호출 됨)
+const Select = (element, index) => {
     element.addEventListener("click", function playerSelect (e) {
-        console.log("select 클릭"); // 작동하는 지 테스트
+        console.log("select 클릭");
 
-        // 주사위 내부 값이 비어있으면 (아무 배열값이라도 0이면 전부 0일테니 0번째만 체크) 리턴
-        // 해당 속성이 맞는지 has어쩌구를 통해 체크 (아니면 리턴해서 선택 무효화)
-        if(diceValue[0] === 0 || !e.target.hasAttribute(`player-0${TURN + 1}`)) { 
-            e.target.addEventListener("click", playerSelect, { once: true }); // once 때문에 한번이라도 실행되었으면 자동 제거되므로 이벤트 리스너 다시 추가
-            return; // 리턴으로 밑 실행 코드 무시
+        if(diceValue[0] === 0 || !e.target.hasAttribute(`player-0${TURN + 1}`) || diceFlag) { 
+            e.target.addEventListener("click", playerSelect, { once: true });
+            return;
         }
     
         e.target.style.backgroundColor = 'grey';
-        diceValue = [0, 0, 0, 0, 0]; // 주사위 값 0으로 리셋
-        rollCount = 3; // 굴릴 수 있는 횟수 리셋
-        
-        document.querySelector(`.gameNumber${TURN + 1}`).style.backgroundColor = 'white';
+        diceValue = [0, 0, 0, 0, 0];
+        rollCount = 3;
 
-        playerboard[TURN][index + 1] = tmpboard[index + 1]; // 스코어에 기록
+        playerboard[TURN][index + 1] = tmpboard[index + 1];
+        if(index + 1 <= 6){
+            playerTotal[TURN].subtotal += tmpboard[index + 1];
+        } else {
+            playerTotal[TURN].maintotal += tmpboard[index + 1];
+        }
 
-        playerTotal[TURN].subtotal += tmpboard[index + 1]; // 최종 토탈에 가산
+        playerTotal[TURN].total = playerTotal[TURN].subtotal + playerTotal[TURN].maintotal
 
-        $$subtotal[TURN].innerHTML = playerTotal[TURN].subtotal; // 서브토탈 부분 보이게
+        $$subtotal[TURN].innerHTML = `${playerTotal[TURN].subtotal}/63`;
+        $$total[TURN].innerHTML = playerTotal[TURN].total;
 
         if(playerTotal[TURN].subtotal >= 63) {
             playerTotal[TURN].bonus = 35;
             $$bonus[TURN].innerHTML = 35;
+            playerTotal[TURN].total += 35;
+        }
+        $$total[TURN].innerHTML = playerTotal[TURN].total;
+
+        const currentPlayerScores = playerboard[TURN];
+        for (let i = 0; i < 12; i++) {
+            if (i !== isNaN(currentPlayerScores[i + 1])) {
+                if(TURN === 0) {
+                    if (!isNaN(playerboard[TURN][i + 1])) continue;
+                    $$player1[i].innerHTML = '';
+                }
+                else if (TURN === 1) {
+                    if (!isNaN(playerboard[TURN][i + 1])) continue;
+                    $$player2[i].innerHTML = '';
+                }
+            }
         }
 
-        TURN += 1; // 턴 + 1
+        document.querySelector(`.gameNumber${TURN + 1}`).style.backgroundColor = 'white';
 
-        // 만약 비어있다면 (예를 들어 TURN이 2일 때 player[2]은 없으니 undefine 일 것임) 실행
+        TURN += 1;
+        selectTurn += 1;
         if(playerboard[TURN] === undefined) TURN = 0;
 
         document.querySelector(`.gameNumber${TURN + 1}`).style.backgroundColor = 'rgba(69, 69, 255, 0.212)';     
 
         $$dice.forEach((element, index) => {
-            element.style.backgroundColor = 'white';
             diceBool[index] = true;
-            element.innerHTML = '';
         })
+
+        if (selectTurn === 24) {
+            let totals = playerTotal.map(player => player.total);
+            let isDraw = totals[0] === totals[1];
+            let winnerName = (totals[0] > totals[1]) ? 'player#1' : 'player#2';
+            let endMessage = isDraw ? "DRAW" :  winnerName + "  Win!" ;
+            document.getElementById('modal').style.display = 'flex'
+            let resultValue = endMessage;
+            let resultElement = document.getElementById("value");
+            resultElement.innerHTML = resultValue;
+        }
+
     }, {once : true});
 }
 
 $$player1.forEach(Select);
 $$player2.forEach(Select);
+
+function keyCodeprint() {
+    var keyCode = window.location.reload()
+    window.status = keyCode
+}
+
+document.querySelector('#restart').addEventListener('click', () => {
+    alert ('게임을 다시하시겠습니까?')
+    keyCodeprint();
+})
+
+function endGame(){
+    let reset = {};
+    if(!reset) return;
+    playerboard = [];
+    document.write(playerboard);
+}
+
+document.querySelector('#end').addEventListener('click', () => {
+    alert ('게임을 종료하시겠습니까?')
+    endGame();
+})
+
+function createDice(number) {
+    const dotPositionMatrix ={
+        1: [
+        [50, 50]
+        ],
+        2: [
+            [20, 20],
+            [80, 80]
+        ],
+        3: [
+            [20, 20],
+            [50, 50],
+            [80, 80]
+        ],
+        4: [
+            [20, 20],
+            [20, 80],
+            [80, 20],
+            [80, 80]
+        ],
+        5: [
+            [20, 20],
+            [20, 80],
+            [50, 50],
+            [80, 20],
+            [80, 80]
+        ],
+        6: [
+            [20, 20],
+            [20, 50],
+            [20, 80],
+            [80, 20],
+            [80, 50],
+            [80, 80]
+        ]
+    };
+
+    const dice = document.createElement("div");
+
+    dice.classList.add("dice");
+
+    for (const dotPosition of dotPositionMatrix[number]){
+        const dot = document.createElement("div");
+
+        dot.classList.add("dice-dot");
+        dot.style.setProperty("--top", dotPosition[0] + "%");
+        dot.style.setProperty("--left", dotPosition[1] + "%");
+
+        dice.appendChild(dot); 
+    }
+    return dice;
+}
+
+function radomizeDice(diceContainer) {
+    diceContainer.innerHTML = "";
+    for (let i = 0; i < 5; i++){
+        let value;
+        if (!diceBool[i]) {
+            value = diceValue[i];
+        } else {
+            value = Math.floor((Math.random()*6)+1);
+            diceValue[i] = value;
+        }
+        const dice = createDice(value);
+        if (!diceBool[i]) {
+            dice.classList.add("lock");
+        }
+        diceContainer.appendChild(dice);
+    }
+}
+
+function setDice(diceContainer) {
+    diceContainer.innerHTML = "";
+    for (let i = 0; i < 5; i++){
+        const dice = createDice(diceValue[i]);
+        dice.addEventListener('click', function () {
+            if (rollCount == 3 || rollCount == 0) return;
+
+            if (dice.classList.contains("lock")) {
+                dice.classList.remove("lock");
+                diceBool[i] = true;
+            } else {
+                if (document.querySelectorAll('.lock').length == 4) return;
+                dice.classList.add("lock");
+                diceBool[i] = false;
+            }
+        });
+        if (!diceBool[i]) {
+            dice.classList.add("lock");
+        }
+        diceContainer.appendChild(dice);
+    }
+}
+
+const diceContainer = document.querySelector(".dice-container");
+setDice(diceContainer);
+diceValue = [0, 0, 0, 0, 0];
